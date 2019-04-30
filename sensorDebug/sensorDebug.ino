1bf0a874fb7ba7f29347203e4b115ace2724b9e5
Derek Withers <derek.withers@aggiemail.usu.edu>
2019-04-30 10:46:05 -0600

    SensorDebug fix

--- sensorDebug/sensorDebug.ino
+++ sensorDebug/sensorDebug.ino
@@ -1,20 +1,24 @@
-#include <Wire.h>
 #include <MPU6050_tockn.h>
+#include <Wire.h>
+
 MPU6050 mpu6050(Wire); //address 0X68 I believe
 
 float p0,p1,p2,p3,p4,p5;
 
 void setup() {
   Serial.begin(9600);
+  Wire.begin();
+  Serial.println("= Sensor Debug =");
+  delay(100);
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(false);
+  Serial.println("= Good Luck!!! =");
   delay(100);
   pinMode(30,OUTPUT);pinMode(31,OUTPUT);
   pinMode(34,OUTPUT);pinMode(35,OUTPUT);
   pinMode(38,OUTPUT);pinMode(39,OUTPUT);
   pinMode(42,OUTPUT);pinMode(43,OUTPUT);
   pinMode(46,OUTPUT);pinMode(47,OUTPUT);
-  delay(100);
-  mpu6050.begin();
-  mpu6050.calcGyroOffsets(true);
 }
 
 void loop() {
